40664118622a5851a547cee506059005eb23d34b
vu.thi.ngoc <vu.thi.ngoc@sun-asterisk.com>
2026-05-22 14:56:10 +0700

    Module 2: Custom Bean and IOC

---
+++ employee-management/src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.example.employeemanagement.controller;
+
+import com.example.employeemanagement.service.UtilityService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+    private final UtilityService utilityService;
+    private final PasswordEncoder passwordEncoder;
+
+    public EmployeeController(UtilityService utilityService, PasswordEncoder passwordEncoder) {
+        this.utilityService = utilityService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/employee/code")
+    public String employeeCode() {
+        return utilityService.generateEmployeeCode();
+    }
+
+    @GetMapping("/employee/name")
+    public String employeeName() {
+        return utilityService.formatEmployeeName("  vu thi ngoc ");
+    }
+
+    @GetMapping("/employee/password")
+    public String employeePassword() {
+        return passwordEncoder.encode("123456");
+    }
+}
